af5771c9bf5a2eb804cad5f06e23f8dae5bbcff9
Ashe Urban <ashe@goldentongue.com>
2022-06-06 15:40:40 -0700

    WIP capitalize first letter.
    Co-authored-by: Garrett Hays <garrettlhays@gmail.com>
    Co-authored-by: CyndaZ42 <github-zacwaggoner42@gmail.com>

--- js/scripts2.js
+++ js/scripts2.js
@@ -1,16 +1,26 @@
 $(document).ready(function() {
   $("form#form").submit(function(event) {
-    const item1 = $("input#item1");
-    const item2 = $("input#item2");
-    const item3 = $("input#item3");
-    const item4 = $("input#item4");
+    event.preventDefault();
+    const item1 = $("input#item1").val();
+    const item2 = $("input#item2").val();
+    const item3 = $("input#item3").val();
+    const item4 = $("input#item4").val();
 
     const groceries = [item1, item2, item3, item4];
     let groceriesFinal = [];
     groceries.sort();
-    groceries.forEach(function(item){
-    groceriesFinal.append(item.toUpperCase());
+    groceries.forEach(function(item) {
+      groceriesFinal += capitalize(item);
     })
     console.log(groceriesFinal);
-  });
+
+   function capitalize(word) {
+   let str;
+   str = word.charAt(0);
+   str.toUpperCase();
+   str += word.slice(1);
+   console.log(str)
+   return str
+   };
+    })
 });
